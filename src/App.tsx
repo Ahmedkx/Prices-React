091c297f4e21ffd0bda7e62de3a0e11408a2c9b6
Ahmedkx <midots202@gmail.com>
2023-08-23 04:34:18 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Loader } from "@mantine/core";
+
 import Layout from "./Layout";
 
 function App() {
-  return (
-    <Layout>
-      <Loader></Loader>asdsss
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Loader></Loader>
+        </Layout>
+    );
 }
 
 export default App;
