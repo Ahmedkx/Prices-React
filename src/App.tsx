e553fd1c4360b107f3574998cb360610d9267811
Ahmedkx <midots202@gmail.com>
2023-08-24 10:28:22 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from "react";
-import "./App.css";
-import { Loader } from "@mantine/core";
-
 import Layout from "./Layout";
+import "./App.css";
+import { Bar } from "react-chartjs-2";
 
 function App() {
+    let data = {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [
+                {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true;
+                };
+            };
+        };
+
+    const optionss = {};
+
     return (
         <Layout>
-            <Loader></Loader>
+            <Bar options={optionss} data={data} />
         </Layout>
     );
 }
