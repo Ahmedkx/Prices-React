81400aac962a9a3bd525cbbfc0a6936fa8311d77
Ahmedkx <midots202@gmail.com>
2023-08-27 01:56:09 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,108 @@
-import Layout from "./Layout";
-import "./App.css";
-import { Bar } from "react-chartjs-2";
+import React, { PureComponent } from "react";
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
+
+const data = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 
 function App() {
-    let data = {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [
-                {
-                    label: "# of Votes",
-                    data: [12, 19, 3, 5, 2, 3],
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true;
-                };
-            };
-        };
-
-    const optionss = {};
-
     return (
-        <Layout>
-            <Bar options={optionss} data={data} />
-        </Layout>
+        <div style={{ width: "1600px", height: "700px" }}>
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <defs>
+                        <linearGradient
+                            id="colorUv"
+                            x1="0"
+                            y1="0"
+                            x2="0"
+                            y2="1"
+                        >
+                            <stop
+                                offset="5%"
+                                stopColor="#0561FC"
+                                stopOpacity={0.17}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="#0561FC"
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
+                    </defs>
+                    <CartesianGrid />
+                    <XAxis dataKey="name" />
+                    <YAxis tickSize={15} tickLine={false} />
+                    <Tooltip />
+                    <Area
+                        type="monotone"
+                        dataKey="uv"
+                        stroke="#9FC1FB"
+                        fill="url(#colorUv)"
+                        strokeWidth="3"
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
     );
 }
 
